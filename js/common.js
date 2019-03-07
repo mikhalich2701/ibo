d2b30f4ebb277e98cc609b3678d77d5739e5e382
mikhalich2701 <mikhalich2701@yandex.ru>
2019-03-07 12:17:44 +0300

    ibo3

--- js/common.js
+++ js/common.js
@@ -4,7 +4,12 @@ $(document).ready(function (){
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
-	  arrows: false
+	  nextArrow: '.header__arrow-right',
+    prevArrow: '.header__arrow-left'
+  });
+
+  $(".header__slide").on('afterChange', function(event, slick, currentSlide){
+      $(".header__row-current-slidenumber").text("0" + (currentSlide + 1));
   });
 
 });
